fc8cb134a39c2639e4d1c0281a3c81f7c1f75f7e
hekaruowo <nikita.chepik.owo@gmail.com>
2023-01-28 21:09:19 +0300

    1

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Accordion from './components/Accordion/Accordion';
+import Rating from './components/Rating/Rating';
+
+
+function App() {
+  return (
+    <div>
+      
+      <input />
+      <input type="date" />
+
+       <AppTitle /> 
+       <Rating /> 
+       <Accordion /> 
+      
+    </div>
+  );
+}
+
+function AppTitle() {
+  return (
+    <> 
+      This is APP component
+    </>
+  )
+}
+
+
+
+
+
+export default App;
---
+++ src/components/Rating/Rating.tsx
@@ -0,0 +1,19 @@
+function Rating() {
+    return (
+      <div>
+          <Star />
+          <Star />
+          <Star />
+          <Star />
+          <Star />
+          <Star />
+      </div>
+    )
+  }
+
+  function Star() {
+    return (
+        <span><b>star </b></span>
+    )
+  }
+  export default Rating;
